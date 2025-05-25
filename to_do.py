4b2e54314f48b4b73cab260a7b2d0625c697d1c2
= <=>
2025-05-25 20:49:06 +0200

    Poprawiono logikę, dodano obsługę Errorów w funkcjach

--- to_do.py
+++ to_do.py
@@ -1,38 +1,49 @@
-tasks = []
+
 
 def show_tasks():
     index = 0
     print()
+    print('Aktualna lista zadań:\n')
+
     for task in tasks:
-        print(task, index)
+        print(f"[{index}], {task}")
         index += 1
 
 def add_task():
     task = input('Podaj zadanie do wykonania: ')
     tasks.append(task)
+    print('Zadanie zostało dopisane.')
     
 def delete_task():
-    deleted = int(input('Które zadanie chcesz usunąć? '))
+    deleted = input('Które zadanie chcesz usunąć? (PODAJ JEGO NUMER) ')
+    
     try:
+        deleted = int(deleted)
         tasks.pop(deleted)
+        print('Zadanie zostało usunięte.')
+    except ValueError:
+        print('Niepoprawny numer zadania!')
     except IndexError:
         print('Nie ma zadania o takim numerze!')
-    return
 
 def save_to_file():
-    with open('tasks.txt', 'a') as file:
+    with open('tasks.txt', 'w') as file:
         for task in tasks:
-            file.write(str(task + '\n'))
+            file.write(task + '\n')
        
     
 def read_from_file():
     with open('tasks.txt', 'r') as file:
         list_of_tasks = file.readlines()
-        print('Aktualna lista zadań:')
+        print('Aktualna lista zadań:\n')
         for _ in list_of_tasks:
             print(_)
-        
 
+try:
+    with open('tasks.txt', 'r') as file:
+        tasks = [line.strip() for line in file.readlines()]
+except FileNotFoundError:
+    tasks = []
         
  
 while True:
@@ -44,30 +55,24 @@ while True:
     print('4. Zapisz zmiany.')
     print('5. Odczytaj dane z pliku.')
     print('6. Wyjdź z programu.')
+    print()
+    choice = input('Co chcesz zrobić? ')
+    print()
     
-    choice = int(input('Co chcesz zrobić? '))
     
-    if choice not in (1,2,3,4,5,6):
+    if choice not in ('1','2','3','4','5','6'):
         print('To nie jest poprawne polecenie dla programu! Wybierz ponownie.\n')
     else:
-        if choice == 1:
+        if choice == '1':
             show_tasks()
-        if choice == 2:
+        if choice == '2':
             add_task()
-        if choice == 3:
+        if choice == '3':
             delete_task()
-        if choice == 4:
+        if choice == '4':
             save_to_file()
-        if choice == 5:
+        if choice == '5':
             read_from_file()
-        if choice == 6:
+        if choice == '6':
             print('Lista zadań zamknięta.')
             break
-            
-    
-    
-            
-
-
-    
-    
